7f29c9b8d67891fd65ca66a417288334b767e009
Diogo Ide <diogoide@gmail.com>
2018-03-08 21:59:49 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,8 @@
 # code your #position_taken? method here!
 def position_taken(board,index)
+  if position_taken == "X"
+    puts = "true"
+  elsif position_taken == " "
+  puts "not taken"
+end
 end
